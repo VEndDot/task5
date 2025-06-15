abe6e21d2da6313f07519d6d7608df6db515874b
Nikolay <nikolya.lysenko@mail.ru>
2025-06-15 22:59:33 +0500

    Add project files.

---
+++ task5/Elephant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("Calling " + Name + ".WhoAmI()");
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My ears are " + EarSize + " inches tall\n");
+
+        }
+    }
+}
---
+++ task5/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace task5
+{
+    class StartElyphan
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+            string userInput = ""; 
+
+            Elephant lloyd = new Elephant();
+            lloyd.Name = "Lloyd";
+            lloyd.EarSize = 40;
+
+            Elephant lucinda = new Elephant();
+            lucinda.Name = "Lucinda";
+            lucinda.EarSize = 33;
+
+            while (userInput != "0")
+            {
+                Console.Write("You pressed ");
+                userInput = Console.ReadLine(); 
+                
+                switch (userInput)
+                {
+                    case "1":
+                        lloyd.WhoAmI();
+                        break;
+                    case "2":
+                        lucinda.WhoAmI();
+                        break;
+                    case "3":
+                        Console.WriteLine("References have been swapped");
+                        Elephant tmpLloyd = lloyd;
+
+                        lloyd = lucinda;
+                        lucinda = tmpLloyd;
+                        break;
+                }
+            }
+            
+
+
+        }
+    }
+}
